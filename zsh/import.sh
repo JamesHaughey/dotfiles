492e61025c3352113aaeeed27e23ccd829a3f69a
James Haughey <jimmyhaugh@googlemail.com>
2019-11-17 12:17:43 +0000

    add zsh options and import/export scripts

---
+++ zsh/import.sh
@@ -0,0 +1,5 @@
+# Copy .zshrc file
+cp ~/.zshrc /mnt/c/projects/dotfiles/zsh/.zshrc
+
+# Copy oh-my-zsh theme
+cp ~/.oh-my-zsh/themes/bullet-train.zsh-theme /mnt/c/projects/dotfiles/zsh/bullet-train.zsh-theme
