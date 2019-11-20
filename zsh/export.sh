edf8a2bbf882b9494529f54ddd9bf2438caa51cc
James Haughey <james.haughey@edfenergy.com>
2019-11-20 19:25:34 +0000

    latest .zshrc

--- zsh/export.sh
+++ zsh/export.sh
@@ -1,5 +1,3 @@
 # Copy .zshrc file
 cp /mnt/c/projects/dotfiles/zsh/.zshrc ~/.zshrc
-
-# Copy oh-my-zsh theme
 cp /mnt/c/projects/dotfiles/zsh/bullet-train.zsh-theme ~/.oh-my-zsh/themes/bullet-train.zsh-theme
